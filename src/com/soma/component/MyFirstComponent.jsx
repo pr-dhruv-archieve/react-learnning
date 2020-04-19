00efb9889a277f61c66d342fa4d6dd7f7bec4f76
pr-dhruv <pra_mahendra@hotmail.com>
2020-04-19 18:18:56 +0530

    First Way of Creating RFC

---
+++ src/com/soma/component/MyFirstComponent.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+/**
+ * This component is stateless functional component.s
+ */
+function MyFirstComponent() {
+    return (
+        <React.Fragment>
+            <h1>Hello, Mahendra</h1>
+            <h2>Mahendra loves sonal.</h2>
+        </React.Fragment>
+    )
+}
+
+export default MyFirstComponent;
