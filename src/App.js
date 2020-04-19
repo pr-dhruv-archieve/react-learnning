1d63e20b297b3e3e391d7e9c8e85b84879ac365f
Mahendra <pra_mahendra@hotmail.com>
2020-04-20 02:57:22 +0530

    CSS Added to Book component

--- src/App.js
+++ src/App.js
@@ -5,9 +5,7 @@ import Books from './com/soma/component/Books';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
         <Books/>
-      </header>
     </div>
   );
 }
--- src/com/soma/component/Books.jsx
+++ src/com/soma/component/Books.jsx
@@ -1,26 +1,28 @@
 import React from 'react';
 import book from '../../../content/book.png'
+import './../css/Books.css'
 
 function Books() {
     return (
-        <section>
+        <>
             <Book />
             <Book />
-        </section>
+            <Book />
+        </>
     )
 };
 
 const Title = () => <h1>My Book</h1>;
 const Author = () => <p>by Author</p>;
-const BookImage = () => <img width="200" height="100" src={book}   />;
+const BookImage = () => <img width="200" height="100" src={book} alt="monkey-donkey"/>;
 
 const Book = () => {
     return (
-        <article>
+        <div className="bookComponent">
             <BookImage />
             <Title/>
             <Author/>
-        </article>
+        </div>
     )
 };
 
